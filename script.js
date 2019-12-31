026ccd7d925f0eac70a7cad87a3c14d7f2bfc27f
Bojo <bozhidar_zahariev@hotmail.com>
2019-12-31 06:44:29 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let frames = document.getElementsByClassName('frames');
 let framesText = document.getElementsByClassName('framesText');
+let mePic = document.getElementById('mePic');
+let back1 = document.getElementById('back1');
 
 Array.from(frames).forEach(item => {
   item.style.color = '#78CDD7';
@@ -11,7 +13,8 @@ frames[0].addEventListener('click', () => {
   frames[3].style.display = 'none';
   frames[0].style.width = '100%';
   frames[0].style.height = '100%';
-  framesText[0].classList.toggle('framesTextUpLeft');
+  framesText[0].classList.add('framesTextUpLeftMove');
+  mePic.style.display = 'none';
 });
 
 frames[1].addEventListener('click', () => {
@@ -20,5 +23,26 @@ frames[1].addEventListener('click', () => {
   frames[3].style.display = 'none';
   frames[1].style.width = '100%';
   frames[1].style.height = '100%';
+  framesText[1].classList.add('framesTextUpRightMove');
+  mePic.style.display = 'none';
 });
 
+frames[2].addEventListener('click', () => {
+  frames[0].style.display = 'none';
+  frames[1].style.display = 'none';
+  frames[3].style.display = 'none';
+  frames[2].style.width = '100%';
+  frames[2].style.height = '100%';
+  framesText[2].classList.add('framesTextDownLeftMove');
+  mePic.style.display = 'none';
+});
+
+frames[3].addEventListener('click', () => {
+  frames[0].style.display = 'none';
+  frames[1].style.display = 'none';
+  frames[2].style.display = 'none';
+  frames[3].style.width = '100%';
+  frames[3].style.height = '100%';
+  framesText[3].classList.add('framesTextDownRightMove');
+  mePic.style.display = 'none';
+});
