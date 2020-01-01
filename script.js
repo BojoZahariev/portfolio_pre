2aa4cfea3f352372c5b61cbb07e8d7baa0bd4be0
Bojo <bozhidar_zahariev@hotmail.com>
2020-01-01 09:06:06 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let frames = document.getElementsByClassName('frames');
-let framesText = document.getElementsByClassName('framesText');
+let frames = document.querySelectorAll('.frames');
+let framesText = document.querySelectorAll('.framesText');
 let mePic = document.getElementById('mePic');
 let back1 = document.getElementById('back1');
 
@@ -7,23 +7,38 @@ Array.from(frames).forEach(item => {
   item.style.color = '#78CDD7';
 });
 
-frames[0].addEventListener('click', () => {
+/*
+const click1 = () => {
+  frames[0].classList.add('framesClickedTopLeft');
   frames[1].style.display = 'none';
   frames[2].style.display = 'none';
   frames[3].style.display = 'none';
-  frames[0].style.width = '100%';
-  frames[0].style.height = '100%';
+
   framesText[0].classList.add('framesTextUpLeftMove');
   mePic.style.display = 'none';
+  back1.style.display = 'block';
+  console.log('ding');
+};
+
+framesText[0].addEventListener('click', click1);
+*/
+
+framesText[0].addEventListener('click', () => {
+  frames[0].classList.add('framesClickedTopLeft');
+  frames[1].style.display = 'none';
+  frames[2].style.display = 'none';
+  frames[3].style.display = 'none';
+  framesText[0].classList.add('framesTextUpLeftMove');
+  mePic.style.display = 'none';
+  back1.style.display = 'block';
 });
 
-frames[1].addEventListener('click', () => {
+framesText[1].addEventListener('click', () => {
   frames[1].classList.add('framesClickedTopRight');
   frames[0].style.display = 'none';
   frames[2].style.display = 'none';
   frames[3].style.display = 'none';
-  frames[1].style.width = '100%';
-  frames[1].style.height = '100%';
+
   framesText[1].classList.add('framesTextUpRightMove');
   mePic.style.display = 'none';
 });
@@ -48,4 +63,14 @@ frames[3].addEventListener('click', () => {
   frames[3].style.height = '100%';
   framesText[3].classList.add('framesTextDownRightMove');
   mePic.style.display = 'none';
+});
+
+back1.addEventListener('click', () => {
+  frames[0].classList.remove('framesClickedTopLeft');
+  framesText[0].classList.remove('framesTextUpLeftMove');
+  frames[1].style.display = 'block';
+  frames[2].style.display = 'block';
+  frames[3].style.display = 'block';
+  mePic.style.display = 'block';
+  back1.style.display = 'none';
 });
